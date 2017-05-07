c20f1f822f50ea9adb51e7fcae28630939d823cd
Stefan Gunnersen <stefan@jul-gunnersen.dk>
2017-05-07 22:02:24 +0200

    Tweaks....

---
+++ CopyDokumenter/Synkroniser_doks.ps1
@@ -0,0 +1,34 @@
+﻿#$source = "D:\Arkiv\Dokumenter" 
+#$dest = "D:\Tresorit\Dokumenter"
+
+if ($args.count -ne 2)
+{
+    Exit
+}
+
+$source = $args[0]
+$dest = $args[1]
+
+if (-Not $source -or -Not $dest) 
+{
+	Exit
+}
+
+$ValidPath = Test-Path $source -IsValid
+
+if ($ValidPath -ne $True)
+{
+    Exit
+}
+
+$ValidPath = Test-Path $dest -IsValid
+
+if ($ValidPath -ne $True)
+{
+    Exit
+}
+
+#Remove-Item -Path "$dest\*" -Recurse
+#robocopy   $source $dest /B /COPYALL /SEC /MIR /R:0 /W:0 /NFL /NDL
+
+robocopy   $source $dest /MIR /XA:H /W:5
